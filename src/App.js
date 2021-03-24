33b9da3a38c7a560b60c3d46565aa547dc9790bf
Thedath Oudarya <tkarunarathne@infomedia.com>
2021-03-24 23:09:39 +0530

    Initial routing implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { default as Home } from "./view/pages/Home";
+import { default as Selector } from "./view/pages/Selector";
+import { default as NotFound } from "./view/pages/NotFound";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/selector">
+        <Selector />
+      </Route>
+      <Route path='*'>
+        <NotFound />
+      </Route>
+    </Switch>
   );
-}
+};
 
 export default App;
---
+++ src/view/pages/Selector/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Selector = () => {
+  return(<>
+    <h1>This is selector page</h1>
+  </>)
+}
+
+export default Selector;
